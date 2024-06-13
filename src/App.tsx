4ede6995ff6601286715d20a78984f59c5a55723
Madison Migliori <madison.migliori@gmail.com>
2024-06-13 11:47:46 -0700

    Updated changeView()

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useId, useState } from "react";
+import { useMemo, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -24,13 +24,13 @@ export interface Task {
 // functions needed:
 
 // addTask(); STATUS: done
-// deleteTask(): STATUS: need help with embedding in TodoCard component
-// handleChange(): STATUS: need help with embedding in TodoCard component
+// deleteTask(): STATUS: done
+// handleChange(): STATUS: not quite done (need line through)
 
-// showAll(); //STATUS: (pseudocode is there) dependent on checkbox
-// showActive();//STATUS: (pseudocode is there) dependent on checkbox
-// showCompleted(); //STATUS: (pseudocode is there) dependent on checkbox
-// clearCompleted();// STATUS: (pseudocode is there) dependent on checkbox
+// showAll(); //STATUS: does not work
+// showActive();//STATUS: does not work
+// showCompleted(); //STATUS: does not work
+// clearCompleted();// STATUS: done
 // showNumTaskLeft() // STATUS: ???
 
 // draggable(); (bonus)
@@ -41,10 +41,16 @@ function App() {
   // () => {}
   // eventFunction
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [views, setViews] = useState("All"); //for the states (ask STEVEN if need this -> filter map (possibly))
-  const [mode, setMode] = useState("darkMode");
+  const [views, setViews] = useState("All"); //for the states
+  const [numTasks, setNumsTask] = useState(0);
+  // const [mode, setMode] = useState("darkMode");
+
+  // let views = ["All", "Active", "Completed"];
 
   // Create variable i.e. filteredTasks = tasks.filter((task) => if(views === blank) {compare task.completed === blank})
+
+  const sortedTasks = useMemo(() => changeView(tasks, views), [tasks, views]);
+  // const numTaskLeft = useMemo(() => showNumTaskLeft(tasks, numTasks), [tasks, numTasks]);
 
   const { register, handleSubmit, reset, formState } = useForm<Schema>({
     resolver: zodResolver(taskSchema),
@@ -71,133 +77,130 @@ function App() {
     setTasks(newTasks);
   };
 
-  const clearCompleted = (id: string) => {
-    if (tasks.filter((task) => id !== task.id)) {
-      const newTasks = tasks.filter((task) => task.completed !== true);
-      setTasks(newTasks);
-    }
-  };
-
-  //changeView
-  // const changeView = () => {
-  //   // All: task,
-  //   // Active:!task.completed,
-  //   // Completed: task.completed,
-  // };
-
-  //TO-DO: need to figure out a way to filter
-  const showAll = () => {
-    setTasks([...tasks]);
-  };
-
-  const showActive = () => {
+  const clearCompleted = () => {
     const newTasks = tasks.filter((task) => task.completed !== true);
     setTasks(newTasks);
   };
 
-  const showCompleted = () => {
-    const newTasks = tasks.filter((task) => task.completed !== false);
-    setTasks(newTasks);
-  };
+  function changeView(tasks: Task, views: string) {
+    return tasks.filter((task: Task) => {
+      if (views === "All") {
+        return true;
+      } else if (views === "Active") {
+        return !task.completed;
+      } else if (views === "Completed") {
+        return task.completed;
+      }
+    });
+  }
 
-  //
+  // function showNumTaskLeft(tasks.completed: boolean) {
+  //   let taskLeft = {tasks.length};
+  //   return tasks.filter((task: Task) => {
+  //     if (task.completed === true) {
+  //       taskLeft = taskLeft - 1;
+  //     } else {
+  //       return taskLeft;
+  //     }
+  //   });
+  // }
 
   console.log(tasks); //use for testing
 
   return (
     <main>
-      <div className="main-container">
-        <picture>
-          <source srcSet="/bg-desktop-dark.jpg" media="(min-width:100px)" />
-          <img src="/bg-desktop-dark.jpg" alt="background-dark" />
-        </picture>
+      <div className="mode">
         <div className="main-container">
-          <div className="todo-container">
-            {/* No To-dos: Have empty Block */}
-            <h1 className="todo">T O D O</h1>
-            <button className="mode">
-              <img className="icon" src="/icon-sun.svg" alt="icon-sun" />
-            </button>
-          </div>
-          <div className="task-container">
-            <div className="create-task">
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <input
-                  {...register("todo")}
-                  id="create-task"
-                  placeholder="Create a new todo.."
-                  type="text"
-                />
-                <span>{formState.errors.todo?.message}</span>
-              </form>
+          <picture>
+            <source srcSet="/bg-desktop-dark.jpg" media="(min-width:100px)" />
+            <img src="/bg-desktop-dark.jpg" alt="background-dark" />
+          </picture>
+          <div className="main-container">
+            <div className="todo-container">
+              {/* No To-dos: Have empty Block */}
+              <h1 className="todo">T O D O</h1>
+              <button className="mode">
+                <img className="icon" src="/icon-sun.svg" alt="icon-sun" />
+              </button>
             </div>
-          </div>
-          <div>
-            <div className="list">
-              <section className="list-container">
-                <ul className="list-items">
-                  {tasks.map((task) => (
-                    <TodoCard
-                      todo={task}
-                      key={task.id}
-                      deleteTask={deleteTask}
-                      handleChange={handleChange}
-                    />
-                  ))}
-
-                  <div className="counter" />
-                  <div className="counter-container">
-                    <p>
-                      {tasks.length} items left{" "}
-                      {tasks.map((task) => (
+            <div className="task-container">
+              <div className="create-task">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <input
+                    {...register("todo")}
+                    id="create-task"
+                    placeholder="Create a new todo.."
+                    type="text"
+                  />
+                  <span>{formState.errors.todo?.message}</span>
+                </form>
+              </div>
+            </div>
+            <div>
+              <div className="list">
+                <section className="list-container">
+                  <ul className="list-items">
+                    {sortedTasks.map((task: Task) => (
+                      <TodoCard
+                        todo={task}
+                        key={task.id}
+                        deleteTask={deleteTask}
+                        handleChange={handleChange}
+                      />
+                    ))}
+                    <div className="counter" />
+                    <div className="counter-container">
+                      <p>
+                        {/* replace task with useMemo */}
+                        {tasks.length} items left{" "}
                         <button
-                          key={task.id}
-                          onClick={() => clearCompleted(task.id)}
+                          onClick={() => clearCompleted()}
                           className="clearCompleted"
                         >
                           Clear Completed
                         </button>
-                      ))}
-                    </p>
-                  </div>
-                </ul>
-              </section>
+                      </p>
+                    </div>
+                  </ul>
+                </section>
+              </div>
             </div>
-          </div>
-          <div className="task">
-            <div className="task-counters">
-              <button className="showAll" onClick={showAll}>
-                All
-              </button>
-              <button className="showActive" onClick={showActive}>
-                Active
-              </button>
-              <button className="showCompleted" onClick={showCompleted}>
-                Completed
-              </button>
+            <div className="task">
+              <div className="task-counters">
+                <button className="showAll" onClick={() => setViews("All")}>
+                  All
+                </button>
+                <button
+                  className="showActive"
+                  onClick={() => setViews("Active")}
+                >
+                  Active
+                </button>
+                <button
+                  className="showCompleted"
+                  onClick={() => setViews("Completed")}
+                >
+                  Completed
+                </button>
+              </div>
             </div>
-          </div>
 
-          <div className="reorder">
-            <p>Drag and drop to reorder list</p>
-          </div>
+            <div className="reorder">
+              <p>Drag and drop to reorder list</p>
+            </div>
 
-          {/* Add dynamic number
-
-    items left All Active Completed Clear Completed Drag and drop to reorder
-    list */}
-
-          <div className="attribution">
-            Challenge by
-            <a
-              href="https://www.frontendmentor.io?ref=challenge"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Frontend Mentor
-            </a>
-            . Coded by{" "}
-            <a href="https://github.com/madisonmigliori/todo-app">Madison</a>.
+            <div className="attribution">
+              Challenge by
+              <a
+                href="https://www.frontendmentor.io?ref=challenge"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Frontend Mentor
+              </a>
+              . Coded by{" "}
+              <a href="https://github.com/madisonmigliori/todo-app">Madison</a>.
+            </div>
           </div>
         </div>
       </div>
